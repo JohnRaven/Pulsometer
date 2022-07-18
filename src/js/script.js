994cc7f59f215f6df1b7c60c13f9dfc38824a402
JohnRaven <John-b-Raven@yandex.ru>
2022-07-19 00:30:04 +0300

    Feature: Add part of the catalog

--- src/js/script.js
+++ src/js/script.js
@@ -16,21 +16,3 @@ $(document).ready(function(){
 	});
 });
 //----------------------------
-
-// JS для tiny-слайдера:
-	// const slider = tns({
-	// 	container: '.carousel__inner',
-	// 	items: 1,
-	// 	slideBy: 'page',
-	// 	autoplay: false,
-	// 	controls: false,
-	// 	nav: false
-	// });
-
-	// document.querySelector('.prev').addEventListener('click', function () {
-	// 	slider.goTo('prev');
-	// });
-	// document.querySelector('.next').addEventListener('click', function () {
-	// 	slider.goTo('next');
-	// });
-//----------------------------
