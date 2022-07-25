d11c79965e704419258dd3028a21549ae3c49dba
JohnRaven <John-b-Raven@yandex.ru>
2022-07-25 23:15:11 +0300

    Feature: Finish  forms

--- src/js/script.js
+++ src/js/script.js
@@ -51,11 +51,21 @@ $(document).ready(function(){
 	toggleSlide('.catalog-item__back');
 
 //Modal
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
 
-	// $('[data-modal=consultation]').on('click', fuction() {
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
 
-	// })
-
+	$('.button_mini').each(function(i){
+		$(this).on('click', function(){
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+// 
 
 });
 //----------------------------
